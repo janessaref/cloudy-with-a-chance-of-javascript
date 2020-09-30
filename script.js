8311d5696227fc34fe5f5425c4df892f150f7a33
Game Over <janessarefong@gmail.com>
2020-09-29 20:34:26 -0700

    attempt clear button in js

--- script.js
+++ script.js
@@ -190,14 +190,11 @@ $(document).ready(function() {
 
         for (var j = 0; j < cityArray.length; j++) {
 
-            // let trashIcon = "<svg width=\"1em\" height=\"1em\" viewBox=\"0 0 16 16\" class=\"bi bi-trash\" fill=\"currentColor\" xmlns=\"http://www.w3.org/2000/svg\"> <path d=\"M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z\"/> <path fill-rule=\"evenodd\" d=\"M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z\"/></svg>"
-
             let cityBtn = $("<button>");
             cityBtn.addClass("newCityBtn");
             cityBtn.addClass("list-group-item");
             cityBtn.addClass("list-group-item-action");
             cityBtn.attr("type", "button");
-            // cityBtn.html(trashIcon);
             cityBtn.attr("data-name", cityArray[j]);
             cityBtn.text(cityArray[j]);
             $("#addCityBtn").prepend(cityBtn);
@@ -244,15 +241,11 @@ $(document).ready(function() {
 
     $(document).on("click", ".newCityBtn", clickButtons);
 
-    // function invalidInput() {
-    //     if ($("#searchInput") == "") {
-    //         $("#error").html("Invalid input");
-    //         $("error").show();
-    //     } else {
-    //         $("error").html("");
-    //         $("error").hide();
-    //     }
-    // }
+    $(".clear").on("click", function() {
+
+        renderButtons.remove();
+        localStorage.clear();
+    });
 });
 
 // let divForecast = $("<div class=\"card text-white bg-info mb-3 mx-auto col-auto>");
